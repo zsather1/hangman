5ca0ad1fd99318550079e86437cc3c9b343f024f
Zach <sather.zach1@gmail.com>
2015-10-10 18:49:24 -0400

    Added some functions and javadocs

--- Hangman/src/hangman/HangmanGame.java
+++ Hangman/src/hangman/HangmanGame.java
@@ -1,8 +1,31 @@
 package hangman;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HangmanGame {
+	
+	private List<String> wordList;
+	
+	public HangmanGame() throws IOException{
+		Words.parseFile("src/words.txt");
+		wordList = Words.getWords();
+		
+	}
+	
+	public boolean isWordGuessed(String word, List<String> guessedLetters){
+		return false;
+	}
+	
+	public String getGuessedWord
+	
+	public boolean is_valid_word(String word ){
+		return false;
+	}
+	
+	public 
+	
+	
 	
 	
 	
